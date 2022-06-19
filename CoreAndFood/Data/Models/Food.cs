e7d4969bc00079c4b96aafee31e38731d8b25c73
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-19 20:44:48 +0300

    Food Class Update

--- CoreAndFood/Data/Models/Food.cs
+++ CoreAndFood/Data/Models/Food.cs
@@ -4,11 +4,9 @@
     {
         public int FoodID { get; set; }
         public string Name { get; set; }
-        public string ShortDescription { get; set; } // Kısa Açıklaması [Vitrin Tarafında Gözükecek Bu Açıklama]
-        public string LongDescription { get; set; } // Uzun Açıklama [Detay Kısmında Gözükecek Bu Açıklama]
+        public string Description { get; set; } 
         public double Price { get; set; }
         public string ImageURL { get; set; }
-        public string ThumbNailImageUrl { get; set; } // Büyük halinin öncesindeki küçük halini tutacak
         public int Stock { get; set; }
 
         public int CategoryID { get; set; }
