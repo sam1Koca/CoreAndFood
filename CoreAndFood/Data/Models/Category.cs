e366c03e3d6bb5967fccb5bf9114cf86a2c55a05
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-19 17:28:10 +0300

    Model Layer Created and Classes was Defined

---
+++ CoreAndFood/Data/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreAndFood.Data.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+    }
+}
---
+++ CoreAndFood/Data/Models/Food.cs
@@ -0,0 +1,16 @@
+﻿namespace CoreAndFood.Data.Models
+{
+    public class Food
+    {
+        public int FoodID { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; } // Kısa Açıklaması [Vitrin Tarafında Gözükecek Bu Açıklama]
+        public string LongDescription { get; set; } // Uzun Açıklama [Detay Kısmında Gözükecek Bu Açıklama]
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public string ThumbNailImageUrl { get; set; } // Büyük halinin öncesindeki küçük halini tutacak
+        public int Stock { get; set; }
+
+
+    }
+}
