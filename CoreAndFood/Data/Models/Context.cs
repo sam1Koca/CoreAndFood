6c3e4c56b730a40246145585543d332d4863d1e5
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-19 19:41:13 +0300

    Context Class Created

---
+++ CoreAndFood/Data/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.Data.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=DbCoreFood; integrated security=true");
+                
+        }
+
+        public DbSet<Food> Foods { get; set; } // Foods Table
+        public DbSet<Category> Categories { get; set; } // Category Table
+
+
+    }
+}
