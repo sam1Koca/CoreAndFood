1dba3a41f5d918bbeeedee2e2f205d6209804e4a
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-20 18:34:59 +0300

    Food and Category Controllers Added!

---
+++ CoreAndFood/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using CoreAndFood.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAndFood.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+
+            return View(categoryRepository.TList());
+        }
+    }
+}
---
+++ CoreAndFood/Controllers/FoodController.cs
@@ -0,0 +1,15 @@
+﻿using CoreAndFood.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAndFood.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            FoodRepository foodRepository = new FoodRepository();
+
+            return View(foodRepository.TList());
+        }
+    }
+}
