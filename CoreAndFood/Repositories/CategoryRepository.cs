5378610fcd26edd34f4c0c4df211b99893b5a048
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-20 14:34:14 +0300

    Category and food repositories created. The methods are written.

---
+++ CoreAndFood/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using CoreAndFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreAndFood.Repositories
+{
+    public class CategoryRepository  // Temel CRUD İşlemlerini Artık Repositoryler üzerinden yapacağız.
+    {
+        Context context = new Context();
+
+        
+
+        public List<Category> CategoryList() {
+
+            return context.Categories.ToList();
+
+        }
+
+        public void CategoryAdd(Category category) {
+
+            context.Categories.Add(category);
+            context.SaveChanges();
+        
+        }
+
+        public void CategoryDelete(Category category) {
+            
+            context.Categories.Remove(category);
+            context.SaveChanges();
+        }
+
+        public void CategoryUpdate(Category category) { 
+
+            context.Categories.Update(category);
+            context.SaveChanges();
+        }
+
+        public void GetCategory(int id) {
+
+            context.Categories.Find(id);
+        }
+
+
+    }
+}
---
+++ CoreAndFood/Repositories/FoodRepository.cs
@@ -0,0 +1,44 @@
+﻿using CoreAndFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreAndFood.Repositories
+{
+    public class FoodRepository
+    {
+        Context context = new Context();
+
+        public void FoodAdd(Food food) {
+
+            context.Foods.Add(food);
+            context.SaveChanges();
+        }
+
+        public void FoodDelete(Food food)
+        {
+
+            context.Foods.Remove(food);
+            context.SaveChanges();
+        }
+
+        public void FoodUpdate(Food food)
+        {
+
+            context.Foods.Update(food);
+            context.SaveChanges();
+        }
+
+        public List<Food> FoodList(Food food)
+        {
+
+            return context.Foods.ToList();
+        }
+
+        public void GetFood(int id)
+        {
+
+            context.Foods.Find(id);
+        }
+
+    }
+}
