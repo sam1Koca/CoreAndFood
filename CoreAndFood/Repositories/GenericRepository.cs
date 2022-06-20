ff64167cb26a13ecaaa4c4d6c4223893c8fed060
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-20 18:02:30 +0300

    Generic Repository Update

---
+++ CoreAndFood/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using CoreAndFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreAndFood.Repositories
+{
+    public class GenericRepository<T> where T : class,new()
+    {
+        Context context = new Context();
+
+
+        public List<T> TList()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void TAdd(T parameter)
+        {
+            context.Set<T>().Add(parameter);
+            context.SaveChanges();
+        }
+
+        public void TDelete(T parameter)
+        {
+            context.Set<T>().Remove(parameter);
+            context.SaveChanges();
+        }
+
+        public void TUpdate(T parameter)
+        {
+            context.Set<T>().Update(parameter);
+            context.SaveChanges();
+        }
+
+        public void TGet(int id)
+        {
+            context.Set<T>().Find(id);
+        }
+    }
+}
