69cdda77767d69c40f9eb4eaac1d830ec768236d
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-20 18:01:29 +0300

    Route Config Set Up

--- CoreAndFood/Startup.cs
+++ CoreAndFood/Startup.cs
@@ -16,6 +16,11 @@ namespace CoreAndFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false); // route configte hata vermemesi için
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +35,21 @@ namespace CoreAndFood
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+
+                app.UseMvc(routes =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Category}/{action=Index}/{id?}");
                 });
+
+
+
             });
         }
     }
