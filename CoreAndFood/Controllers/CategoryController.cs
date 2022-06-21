97fbf006a453f1a9e2886055707aff50500ebec9
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-21 15:53:48 +0300

    Category Addition Process.

--- CoreAndFood/Controllers/CategoryController.cs
+++ CoreAndFood/Controllers/CategoryController.cs
@@ -1,15 +1,32 @@
 ﻿using CoreAndFood.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using CoreAndFood.Data.Models;
 
 namespace CoreAndFood.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryRepository categoryRepository = new CategoryRepository(); // CategoryRepository Classına ulaşmam gerek. (GlobalObject)
+
         public IActionResult Index()
         {
-            CategoryRepository categoryRepository = new CategoryRepository();
+            
 
             return View(categoryRepository.TList());
+        }
+
+        [HttpGet]
+        public IActionResult CategoryAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CategoryAdd(Category category)
+        {
+            
+            categoryRepository.TAdd(category);
+            return RedirectToAction("Index");
         }
     }
 }
