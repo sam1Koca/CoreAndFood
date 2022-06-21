42c83076be5266d61a05a1916346c0ba0e634c10
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-21 17:22:55 +0300

    Adding Food Written

--- CoreAndFood/Controllers/FoodController.cs
+++ CoreAndFood/Controllers/FoodController.cs
@@ -1,15 +1,29 @@
 ﻿using CoreAndFood.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using CoreAndFood.Data.Models;
 
 namespace CoreAndFood.Controllers
 {
     public class FoodController : Controller
     {
+        FoodRepository foodRepository = new FoodRepository(); // Global Object
+
         public IActionResult Index()
         {
-            FoodRepository foodRepository = new FoodRepository();
-
             return View(foodRepository.TList("Category"));
+        }
+
+        [HttpGet]
+        public IActionResult AddFood()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFood(Food food)
+        {
+            foodRepository.TAdd(food);
+            return RedirectToAction("Index");
         }
     }
 }
