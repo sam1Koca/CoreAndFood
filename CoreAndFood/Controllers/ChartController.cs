bb37be01d01f6ef2b53ac4dc2c7d1b371dd0e64c
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-24 23:36:44 +0300

    Static Google Chart Added

---
+++ CoreAndFood/Controllers/ChartController.cs
@@ -0,0 +1,48 @@
+﻿using CoreAndFood.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreAndFood.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult VisualizeProductResult()
+        {
+            return Json(ProductList());
+        }
+
+        public List<Class1> ProductList()
+        {
+           List<Class1> productList = new List<Class1>();
+            productList.Add(new Class1()
+            {
+                productName = "Computer",
+                stock = 120
+            });
+
+            productList.Add(new Class1()
+            {
+                productName = "Keyboard",
+                stock = 45
+            });
+
+            productList.Add(new Class1()
+            {
+                productName = "Mouse",
+                stock = 220
+            });
+
+            return productList;
+        }
+    }
+}
