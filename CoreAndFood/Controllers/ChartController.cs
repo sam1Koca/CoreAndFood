0ef3071f9cd37884c69182ecbf902dd5b268d149
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-25 22:22:36 +0300

    Google Dynamic Chart Added!

--- CoreAndFood/Controllers/ChartController.cs
+++ CoreAndFood/Controllers/ChartController.cs
@@ -1,6 +1,9 @@
 ﻿using CoreAndFood.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using CoreAndFood.Data;
+using CoreAndFood.Data.Models;
+using System.Linq;
 
 namespace CoreAndFood.Controllers
 {
@@ -23,7 +26,7 @@ namespace CoreAndFood.Controllers
 
         public List<Class1> ProductList()
         {
-           List<Class1> productList = new List<Class1>();
+            List<Class1> productList = new List<Class1>();
             productList.Add(new Class1()
             {
                 productName = "Computer",
@@ -44,5 +47,27 @@ namespace CoreAndFood.Controllers
 
             return productList;
         }
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+        public List<Class2> FoodList()
+        {
+            List<Class2> class2 = new List<Class2>();
+            using(var context = new Context())
+            {
+                class2 = context.Foods.Select(x => new Class2() { foodName = x.Name, stock = x.Stock }).ToList();
+                return class2;
+            }
+        }
+
+        public IActionResult VisualizeProductResult2()
+        {
+            return Json(FoodList());
+        }
+
+        
+        
     }
 }
---
+++ CoreAndFood/Data/Class2.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreAndFood.Data
+{
+    public class Class2
+    {
+        public string foodName { get; set; }
+        public int stock { get; set; }
+
+    }
+}
